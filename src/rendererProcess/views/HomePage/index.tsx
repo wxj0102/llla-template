e2dd725cff6609aec8cdb935b039508257f3bb1c
wxj0102 <740302323@qq.com>
2019-11-26 16:59:49 +0800

    antd

--- src/rendererProcess/views/HomePage/index.tsx
+++ src/rendererProcess/views/HomePage/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Button } from 'antd';
 import styles from './index.module.scss';
 
 const App: React.FC = () => {
   return (
     <div className={styles.container}>
-      我是首页啊
+      <Button>我是首页啊</Button>
     </div>
   );
 }
