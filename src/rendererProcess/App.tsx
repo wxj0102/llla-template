646ea3acd751348b318d0e8dcc8918472ac92a8d
wxj0102 <740302323@qq.com>
2019-11-26 11:32:24 +0800

    temp

---
+++ src/rendererProcess/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './common.css';
+import HomePage from './views/HomePage';
+
+const App: React.FC = () => {
+  return (
+    <div className="app-container">
+      <HomePage></HomePage>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/rendererProcess/views/HomePage/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <div className="container">
+      大发大东方阿道夫阿斯蒂芬阿斯蒂芬阿斯蒂芬安防打算sad
+    </div>
+  );
+}
+
+export default App;
